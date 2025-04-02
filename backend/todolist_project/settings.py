22895ea2b4794e500dd8aa47d17f43e4a9a29c0e
Jimmy Lee <jimmylee850328@gmail.com>
2025-04-02 17:05:06 +0800

    Feat: Update Docker and Django settings for PostgreSQL integration
    - Modified docker-compose.yml to remove trailing slash from PostgreSQL data volume path.
    - Updated Django settings.py to switch database engine to PostgreSQL and configure database connection details.

--- backend/todolist_project/settings.py
+++ backend/todolist_project/settings.py
@@ -88,8 +88,12 @@ WSGI_APPLICATION = 'todolist_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'todolist',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
